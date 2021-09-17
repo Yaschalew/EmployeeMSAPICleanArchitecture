341e2c230d9329f2a16ce4071839ebf5c065e325
Ashenafi-Fiseha <AFiseha@excellerent.com>
2021-09-17 11:07:52 +0300

    attendance 2

--- EmployeeMSAPICleanArchitecture/Applications/Mappings/AttendancesMapper.cs
+++ EmployeeMSAPICleanArchitecture/Applications/Mappings/AttendancesMapper.cs
@@ -36,53 +36,5 @@ namespace Applications.Mappings
                 Remark = attendance.Remark
             };
         }
-
-        public static AttendanceItem MapperSingle(Attendance attendance)
-        {
-            return new AttendanceItem
-            {
-                ID = attendance.ID,
-                Employee = attendance.Employee,
-                LoginTime = attendance.LoginTime,
-                LoginDescription = attendance.LoginDescription,
-                LogoutTime = attendance.LogoutTime,
-                LogoutDescription = attendance.LogoutDescription,
-                Remark = attendance.Remark
-            };
-        }
-        public static Attendance MapperSingle(AttendanceItem attendance)
-        {
-            return new Attendance
-            {
-                ID = attendance.ID,
-                Employee = attendance.Employee,
-                LoginTime = attendance.LoginTime,
-                LoginDescription = attendance.LoginDescription,
-                LogoutTime = attendance.LogoutTime,
-                LogoutDescription = attendance.LogoutDescription,
-                Remark = attendance.Remark
-            };
-        }
-
-        public static IEnumerable<Attendance> MapperCollection(IEnumerable<AttendanceItem> attendanceItems)
-        {
-            List<AttendanceItem> attendanceItemsList = new List<AttendanceItem>(attendanceItems);
-            List<Attendance> attendanceModelsList = new List<Attendance>();
-            for (int i = 0; i < attendanceItemsList.Count; i++)
-            {
-                attendanceModelsList.Add(attendanceItemsList[i].ToModel());
-            }
-            return attendanceModelsList;
-        }
-        public static IEnumerable<AttendanceItem> MapperCollection(IEnumerable<Attendance> attendanceModels)
-        {
-            List<AttendanceItem> attendanceItemsList = new List<AttendanceItem>();
-            List<Attendance> attendanceModelsList = new List<Attendance>(attendanceModels);
-            for (int i = 0; i < attendanceModelsList.Count; i++)
-            {
-                attendanceItemsList.Add(attendanceModelsList[i].ToDto());
-            }
-            return attendanceItemsList;
-        }
     }
 }
